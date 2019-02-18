347d6104123f6d99a4b23166ac542cafb1e5ae00
atomsk <bigman01kr@gmail.com>
2019-02-18 14:33:22 +0900

    2739, 2741, 2742 for loop

---
+++ src/algorithm101/Algorithm03_0218_2438.java
@@ -0,0 +1,26 @@
+package algorithm101;
+
+import java.util.Scanner;
+
+public class Algorithm03_0218_2438 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        for (int i = 1; i <= N; i++){
+
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+
+    }
+
+}
