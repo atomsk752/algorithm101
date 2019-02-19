d06e710a24c10e0b292f7b7bb110f4eb21166d11
atomsk <bigman01kr@gmail.com>
2019-02-19 11:26:58 +0900

    2440 nested loop

---
+++ src/algorithm101/Algorithm03_0219_2440.java
@@ -0,0 +1,28 @@
+package algorithm101;
+
+import java.util.Scanner;
+
+public class Algorithm03_0219_2440 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        for (int j = 0; j < N; j++) {
+
+            for (int i = 0; i < N-j; i++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+        }
+
+
+    }
+}
