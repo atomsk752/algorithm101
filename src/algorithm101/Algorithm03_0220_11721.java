4592e057ad51e0ae0be8fb7a2e4f00e3a60bfc2d
atomsk <bigman01kr@gmail.com>
2019-02-20 20:44:04 +0900

    11721 charAt

---
+++ src/algorithm101/Algorithm03_0220_11721.java
@@ -0,0 +1,24 @@
+package algorithm101;
+
+import java.util.Scanner;
+
+public class Algorithm03_0220_11721 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String word = scan.next();
+
+        for (int i = 0; i < word.length(); i++) {
+
+            System.out.print(word.charAt(i));
+
+            if (i % 10 == 9){
+                System.out.println();
+            }
+
+        }
+
+    }
+}
