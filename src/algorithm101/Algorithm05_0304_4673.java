1377bb60f0e6bdad7ea29f9b74346ca9a8a83f32
atomsk752 <bigman01kr@gmail.com>
2019-03-04 15:38:01 +0900

    4673

---
+++ src/algorithm101/Algorithm05_0304_4673.java
@@ -0,0 +1,43 @@
+package algorithm101;
+
+public class Algorithm05_0304_4673 {
+
+    public static void main(String[] args) {
+
+        boolean[] selfNumArr = new boolean[10036];
+
+        for (int i = 1; i < 10000; i++) {
+
+            selfNumArr[getDn(i)] = true;
+
+        }
+
+        for (int i = 1; i < selfNumArr.length; i++) {
+
+            if (!selfNumArr[i] && i<=10000){
+
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
+
+    private static int getDn(int n){
+
+        int dn = n;
+
+        while(n>0){
+
+            dn += n % 10;
+            n /= 10;
+
+        }
+
+        return dn;
+
+    }
+
+
+}
