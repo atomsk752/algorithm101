2f61e6d83b141c6abfff509d9f1ad7cbb02c3bb8
atomsk752 <bigman01kr@gmail.com>
2019-03-05 17:39:24 +0900

    2448 need more practice

---
+++ src/algorithm101/baekjoon05/Algorithm05_0305_2448.java
@@ -0,0 +1,62 @@
+package algorithm101.baekjoon05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algorithm05_0305_2448 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String map[] = new String[N];
+
+        map[0] = "  *  ";
+        map[1] = " * * ";
+        map[2] = "*****";
+
+        for (int k = 1; 3 * Math.pow(2, k) <= N; k++){
+
+            makeStar(k, map);
+        }
+
+
+        for (int i = 0; i < N; i++) {
+
+            System.out.println(map[i]);
+
+        }
+
+
+    }
+
+    private static void makeStar(int k, String map[]){
+
+        int bottom = 3 * (int)Math.pow(2, k);
+        int middle = bottom / 2;
+
+        for (int i = middle; i < bottom; i++) {
+
+            map[i] = map[i - middle] + " " + map[i - middle];
+
+        }
+
+        String space = "";
+
+        while (space.length()<middle){
+
+            space += " ";
+        }
+
+        for (int i = 0; i < middle; i++) {
+
+            map[i] = space + map[i] + space;
+
+        }
+
+    }
+
+}
