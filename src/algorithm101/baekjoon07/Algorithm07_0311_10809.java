1ffa48cda327fd41e6ef065805eb72186b13859b
atomsk752 <bigman01kr@gmail.com>
2019-03-12 01:54:17 +0900

    10809

---
+++ src/algorithm101/baekjoon07/Algorithm07_0311_10809.java
@@ -0,0 +1,16 @@
+package algorithm101.baekjoon07;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Algorithm07_0311_10809 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+
+}
