1f662d38652fb29acf2ef080e94cd9936c3b977f
atomsk752 <bigman01kr@gmail.com>
2019-03-11 23:35:08 +0900

    11654 string to int

---
+++ src/algorithm101/baekjoon07/Algorithm07_0311_11654.java
@@ -0,0 +1,19 @@
+package algorithm101.baekjoon07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Algorithm07_0311_11654 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int result = br.readLine().charAt(0);
+
+        System.out.println(result);
+
+
+    }
+}
